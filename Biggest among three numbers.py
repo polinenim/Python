fabf1fc28b025ff02211d4c8c14b00bad391a040
Mahesh Polineni <79160137+polinenim@users.noreply.github.com>
2022-02-17 23:37:17 +0530

    Delete Biggest among three numbers.py

--- Biggest among three numbers.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter a number"))
-b=int(input("enter a number"))
-c=int(input("enter a number"))
-if (a>b and a>c):
-   print(a,"is biggest")
-elif (b>a and b>c):
-     print(b,"is biggest")
-elif (c>a and c>a):
-     print (c,"is biggest")

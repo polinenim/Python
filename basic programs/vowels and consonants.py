6da5dbe46689f314f5d7a096ca7c36b196b43c94
Mahesh Polineni <maheshpolineni1008@gmail.com>
2022-02-19 22:55:41 +0530

    print vowels and consonants in given name

---
+++ basic programs/vowels and consonants.py
@@ -0,0 +1,8 @@
+a=(input("enter a name"))
+for i in (a):
+    if i in("a,e,i,o,u"):
+        print(i,"vowels")
+    elif i in("A,E,I,O,U"):
+         print(i,"vowels")
+    else:
+         print(i,"consonants")

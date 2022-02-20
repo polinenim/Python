4e1b8f125ddf2d7620ab52fc35773db1e3524d44
Mahesh Polineni <79160137+polinenim@users.noreply.github.com>
2022-02-20 23:10:51 +0530

    Use the operators for sum,subtraction,division,multiplication

--- basic programs/operators.py
+++ basic programs/operators.py
@@ -2,10 +2,10 @@ a=int(input("enter a number"))
 b=int(input("enter a number"))
 o=(input("enter a operator code"))
 if o=='+':
-    print("sum is",a+b)
+    print("sum is:",a+b)
 if o=='-':
-    print("subtraction is",a-b)
+    print("subtraction is:",a-b)
 if o=='*':
-    print("multiplication is",a*b)
+    print("multiplication is:",a*b)
 if o=='/':
-    print("devision is",a/b)
+    print("devision is:",a/b)
